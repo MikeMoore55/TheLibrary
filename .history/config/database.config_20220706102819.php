3e459d1348d0e1b5a1c2256a9ef2eb129458c2f4
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-06 10:30:46 +0200

    data base config aded, and footer and header includes added
    added the data base config file and functionality
    added footer file
    added header file

---
+++ .history/config/database.config_20220706102819.php
@@ -0,0 +1,26 @@
+<!-- config file for database -->
+<?php
+
+    function connect(){
+
+        $host = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbName = "MyLibrary";
+    
+    
+        // Create connection object
+        $conn = new mysqli($host, $username, $password, $dbName);
+    
+        // Check connection
+        if ($conn->connect_error) {
+            //close connection in case of error
+            die("Connection failed: db " . $conn->connect_error); 
+        } 
+        else {
+            return $conn;
+        }  
+
+    }
+
+?>
