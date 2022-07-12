b4f3434fb164f2e977d9dd15403ccfd2fb5bf596
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-12 07:02:50 +0200

    comments added

--- .history/src/public/pages/update-book.page_20220712065653.php
+++ .history/src/public/pages/update-book.page_20220712065653.php
@@ -1,24 +1,33 @@
-<!-- page for librarian to add new book to database -->
+<!-- page for librarian to update any book on database -->
 
-<!-- 
-    >>> naming convention <<<
-
-    - name-name === html, js, css
-    - name_name === php, sql
-
--->
 <?php
-    session_start();
 
     include "/MAMP/htdocs/TheLibrary/config/database.config.php";
-    //connect to db
+
     $conn = connect();
+    // getting all info of books
+    $sql_1 = "SELECT * FROM books"; // sql statement 1
 
-    // set variables to blank
-    $book_name = $book_author = $book_year = $book_genre = "";
-    $book_name_error = $book_author_error = $book_year_error = $book_genre_error = "";
+    $result = $conn->query($sql_1);
 
+    if ($result->num_rows > 0) {
+
+        while($row = $result->fetch_assoc()) {
+            // display book option for updating        
+            $book_options .= '
+                                <option>
+                                '.$row["book_name"].'
+                                </option>';
+                        
+        };
+
+    } ;
+
+    
     if($_SERVER["REQUEST_METHOD"]== "POST"){
+        // to use the selected option as a "key"
+        $selected_book = $_POST["selected-book"];
+
         // -- initiate variables first -- //
 
         // initiate book name
@@ -61,47 +70,56 @@
             $book_genre = trim($_POST["book_genre"]);
         }
 
-        // sql insert statement
+        // sql statement 2 for updating
+        $sql_2 = "UPDATE `books` SET `book_name` = '$book_name', `book_year` = '$book_year', `book_genre` = '$book_genre'  WHERE `books`.`book_name` = '$selected_book'";
 
-        $sql .= "INSERT INTO books (book_name, book_author, book_year, book_genre) VALUES ('$book_name', '$book_author', '$book_year', '$book_genre')";
-          
-        // when successful, user will be taken back to librarian page, and new book will be added to list
-        if ($conn->multi_query($sql) === TRUE) {
+          // when successful, user will be taken back to librarian page, and new book will be added to list
+          if ($conn->multi_query($sql_2) === TRUE) {
             header("location: librarian") ;
         }
         else {
-/*             echo '<script>alert("Error, could not add book, please try again")</script>';
- */        };
-        
-        //close connection
-        $conn->close();
+            /* echo '<script>alert("Error, could not update book, please try again")</script>'; */
+        };
     }
+
+    // close connection
+    $conn -> close();
+
 ?>
-<main class="add-book">
-    <!-- add book form -->
-    <form class="add-form" method="POST">
-        <span id="add-close" class="close-btn">X</span>
-        <h3>New Book</h3>
+
+<main class="update">
+    <form class="update-form" method="POST">
+        <span id="update-close" class="close-btn">X</span>
+        <h3>Update Book</h3>
+        <label for="book-option" class="form-label">
+            Select the book you wish to edit/update:
+        </label>
+        <select id="book-option" class="form-control" name="selected-book">
+            <?php
+                echo $book_options;
+            ?>
+        </select>
         <label for="book-name" class="form-label">
-            book name:
+            enter book name:
         </label>
         <input id="book-name" type="text" class="form-control" name="book_name">
         <div class="invalid-feedback">
             Please fill in your username
         </div>
         <label for="author" class="form-label">
-            author:
+            enter author:
         </label>
         <input id="author" type="text" class="form-control" name="book_author">
         <label for="released-date" class="form-label">
-            year released:
+            enter year released:
         </label>
         <input id="released-date" type="text" class="form-control"  name="book_year">
         <label for="genre" class="form-label">
-            genre:
+            enter genre:
         </label>
         <input id="genre" type="text" class="form-control"  name="book_genre">
         <br>
-        <input id="add" type="submit" class="btn btn-primary mb-3 btn-override" name="add" value="Add">
+        <p>(Please ensure all fields are filled out and new info is entered accordingly)</p>
+        <input id="add" type="submit" class="btn btn-primary mb-3 btn-override" name="update" value="Update">
     </form>
 </main>
